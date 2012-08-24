1d631f15696ebfa259f801a2c1a81c490890d228
Gert Vrebos <gert.vrebos@tactics.be>
2012-08-24 11:50:21 +0200

    Added 'large' option for chosen and updated docs

--- Resources/public/js/tacticsform-chosen.js
+++ Resources/public/js/tacticsform-chosen.js
@@ -1,3 +1,7 @@
 jQuery(document).ready(function($) {
     $(".chosen").chosen();
+    
+    $(".chosen").each(function(){
+        $(this).next('.chzn-container').addClass($(this).attr('chosen-class'));
+    });
 });
