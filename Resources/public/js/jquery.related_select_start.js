b1917625e5aaca3417672fdfee4a96ee11be2402
Jeroen <jeroen@tactics.com>
2012-08-29 10:29:15 +0200

    fix voor laatste veld organisatie select

--- Resources/public/js/jquery.related_select_start.js
+++ Resources/public/js/jquery.related_select_start.js
@@ -4,7 +4,7 @@ $(function(){
       onChangeLoad: 'refreshData',
       loadingMessage: 'Please wait',
       defaultOptionText: ' -- Choose an option -- ',
-      selects: ['OrganisationType[bussiness_unit_id]', 'OrganisationType[plant_id]', 'OrganisationType[division_id]','OrganisationType[department_id]','OrganisationType[area_id]','OrganisationType[equipment_id]']
+      selects: ['OrganisationType[bussiness_unit_id]', 'OrganisationType[plant_id]', 'OrganisationType[division_id]','OrganisationType[department_id]','OrganisationType[area_id]','OrganisationType[equipment_type_id]']
       });
 });
 
