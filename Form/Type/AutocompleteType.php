39e79702582856da652d11ec7f08613bca789b4f
Bart Ruysseveldt <bart@tactics.be>
2013-04-11 16:05:06 +0200

    [TASK] Autocomplete form type

--- Form/Type/AutocompleteType.php
+++ Form/Type/AutocompleteType.php
@@ -3,22 +3,45 @@ namespace Tactics\Bundle\FormBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class AutocompleteType extends AbstractType
 {
-    public function getDefaultOptions(array $options)
+    /**
+     * {@inheritDoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        return array(
-                'widget' => 'single_text',
-                'format' => 'dd/MM/yyyy',
-                'attr' => array(
-                    'class' => 'datepicker'
-                ),
-                'masked_input' => array(
-                  'mask' => '99/99/9999',
-                  'placeholder' => ' ',
-                )
-            );
+        $view->vars['route'] = $options['route'];
+        $view->vars['route_params'] = $options['route_params'];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setRequired(array(
+            'class',
+            'route'
+        ))
+        ->setOptional(array(
+            'em',
+            'route_params'
+        ))
+        ->setDefaults(array(
+            'em' => 'default',
+            'route_params' => array(),
+            'attr' => array(
+                'style' => 'display: none;'
+            )
+        ))
+        ->setAllowedTypes(array(
+            'route_params' => 'array'
+        ));
     }
 
     public function getParent()
