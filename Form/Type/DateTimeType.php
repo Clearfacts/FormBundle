2a037b555151b1b03f6b320894fce5b1304d244c
Bart Ruysseveldt <bart@tactics.be>
2013-11-06 16:33:59 +0100

    updated jquery.ui.timepicker to latest version and fixed css

--- Form/Type/DateTimeType.php
+++ Form/Type/DateTimeType.php
@@ -2,22 +2,6 @@
 namespace Tactics\Bundle\FormBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
-
-// temp list
-use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormView;
-use Symfony\Component\Form\ReversedTransformer;
-use Symfony\Component\Form\Extension\Core\DataTransformer\DataTransformerChain;
-use Symfony\Component\Form\Extension\Core\DataTransformer\DateTimeToArrayTransformer;
-use Symfony\Component\Form\Extension\Core\DataTransformer\DateTimeToStringTransformer;
-use Symfony\Component\Form\Extension\Core\DataTransformer\DateTimeToLocalizedStringTransformer;
-use Symfony\Component\Form\Extension\Core\DataTransformer\DateTimeToTimestampTransformer;
-use Symfony\Component\Form\Extension\Core\DataTransformer\DateTimeToRfc3339Transformer;
-use Symfony\Component\Form\Extension\Core\DataTransformer\ArrayToPartsTransformer;
-use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class DateTimeType extends AbstractType
@@ -25,13 +9,15 @@ class DateTimeType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getDefaultOptions(array $options)
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        return array(
-            'date_format' => 'dd/MM/yyyy', 
-            'date_widget' => 'single_text', 
+        $resolver->setDefaults(array(
+            'date_format' => 'dd/MM/yyyy',
+            'date_widget' => 'single_text',
             'time_widget' => 'single_text',
-            'attr' => array('class' => 'tactics_datetime'),
+            'attr' => array(
+                'class' => 'tactics_datetime'
+            ),
             'date_options' => array(
                 'masked_input' => array(
                     'mask' => '99/99/9999',
@@ -42,11 +28,9 @@ class DateTimeType extends AbstractType
                 'masked_input' => array(
                     'mask' => '99:99',
                     'placeholder' => ' ',
-                ),
-                'attr' => array('class' => 'jox')
-            ),
-
-        );
+                )
+            )
+        ));
     }
 
     /**
